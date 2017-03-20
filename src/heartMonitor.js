90243823158b58feb34f4512018394481a798876
Alabê Duarte <alabeduarte@gmail.com>
2017-03-20 12:06:59 +1100

    Add non zero evaluation support to identify a flat line. When the signal remains the same, it's status should be "Stopped Beating".

--- src/heartMonitor.js
+++ src/heartMonitor.js
@@ -1,11 +1,23 @@
 const STATUS = require('./heartMonitorStatus');
 
 class HeartMonitor {
+  constructor(featureToggle) {
+    this.featureToggle = featureToggle;
+  }
+
   getStatus (dataIntervals) {
-    const allSignals = dataIntervals.map( (data) => data.signal );
-    const flatLine = allSignals.every( (signal) => signal === 0 );
+    const flatLine = this.isFlatLine(dataIntervals);
 
     return flatLine ? STATUS.STOPPED_BEATING : STATUS.HEART_BEATING;
+  }
+
+  isFlatLine (dataIntervals) {
+    const allSignals = dataIntervals.map( (data) => data.signal );
+
+    return allSignals.every( (signal) => {
+      return this.featureToggle.stopBeatingWhenSignalRemainsTheSame ?
+        signal === allSignals[0] : signal === 0;
+    });
   }
 }
 
--- test/heartMonitorTest.js
+++ test/heartMonitorTest.js
@@ -5,9 +5,11 @@ const HeartMonitor = require('../src/heartMonitor');
 describe('HeartMonitor', () => {
   const StoppedBeating = STATUS.STOPPED_BEATING;
   const HeartBeating = STATUS.HEART_BEATING;
-  const heartMonitor = new HeartMonitor();
+  const featureToggle = { stopBeatingWhenSignalRemainsTheSame: false };
 
-  it('indicates that heart has stopped beating when signal remains the same', () => {
+  const heartMonitor = new HeartMonitor(featureToggle);
+
+  it('indicates that heart has stopped beating when all signals are zero', () => {
     const dataIntervals = [
       { timestamp: '2017-03-16T06:00', signal: 0 },
       { timestamp: '2017-03-16T06:30', signal: 0 },
@@ -25,5 +27,30 @@ describe('HeartMonitor', () => {
     ];
 
     expect(heartMonitor.getStatus(dataIntervals)).to.eql(HeartBeating);
+  });
+
+  describe('when toggle is ON', () => {
+    const featureToggle = { stopBeatingWhenSignalRemainsTheSame: true };
+    const heartMonitor = new HeartMonitor(featureToggle);
+
+    it('indicates that heart has stopped beating when signal remains the same', () => {
+      const dataIntervals = [
+        { timestamp: '2017-03-16T06:00', signal: 1 },
+        { timestamp: '2017-03-16T06:30', signal: 1 },
+        { timestamp: '2017-03-16T07:00', signal: 1 },
+      ];
+
+      expect(heartMonitor.getStatus(dataIntervals)).to.eql(StoppedBeating);
+    });
+
+    it('indicates that heart is beating', () => {
+      const dataIntervals = [
+        { timestamp: '2017-03-16T06:00', signal: 1 },
+        { timestamp: '2017-03-16T06:30', signal: 0 },
+        { timestamp: '2017-03-16T07:00', signal: 1 },
+      ];
+
+      expect(heartMonitor.getStatus(dataIntervals)).to.eql(HeartBeating);
+    });
   });
 });
