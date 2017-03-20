d40d68b7586e7cf15fdb81c61c22b3a11b2bcb03
Alabê Duarte <alabeduarte@gmail.com>
2017-03-20 12:19:55 +1100

    Enable toggle by default on test scenarios which reduces the number of scenarios required to keep compatibility with the old code

--- test/heartMonitorTest.js
+++ test/heartMonitorTest.js
@@ -5,19 +5,9 @@ const HeartMonitor = require('../src/heartMonitor');
 describe('HeartMonitor', () => {
   const StoppedBeating = STATUS.STOPPED_BEATING;
   const HeartBeating = STATUS.HEART_BEATING;
-  const featureToggle = { stopBeatingWhenSignalRemainsTheSame: false };
+  const featureToggle = { stopBeatingWhenSignalRemainsTheSame: true };
 
   const heartMonitor = new HeartMonitor(featureToggle);
-
-  it('indicates that heart has stopped beating when all signals are zero', () => {
-    const dataIntervals = [
-      { timestamp: '2017-03-16T06:00', signal: 0 },
-      { timestamp: '2017-03-16T06:30', signal: 0 },
-      { timestamp: '2017-03-16T07:00', signal: 0 },
-    ];
-
-    expect(heartMonitor.getStatus(dataIntervals)).to.eql(StoppedBeating);
-  });
 
   it('indicates that heart is beating', () => {
     const dataIntervals = [
@@ -29,24 +19,34 @@ describe('HeartMonitor', () => {
     expect(heartMonitor.getStatus(dataIntervals)).to.eql(HeartBeating);
   });
 
-  describe('when toggle is ON', () => {
-    const featureToggle = { stopBeatingWhenSignalRemainsTheSame: true };
+  it('indicates that heart has stopped beating when all signals are zero', () => {
+    const dataIntervals = [
+      { timestamp: '2017-03-16T06:00', signal: 0 },
+      { timestamp: '2017-03-16T06:30', signal: 0 },
+      { timestamp: '2017-03-16T07:00', signal: 0 },
+    ];
+
+    expect(heartMonitor.getStatus(dataIntervals)).to.eql(StoppedBeating);
+  });
+
+  it('indicates that heart has stopped beating when signal remains the same', () => {
+    const dataIntervals = [
+      { timestamp: '2017-03-16T06:00', signal: 1 },
+      { timestamp: '2017-03-16T06:30', signal: 1 },
+      { timestamp: '2017-03-16T07:00', signal: 1 },
+    ];
+
+    expect(heartMonitor.getStatus(dataIntervals)).to.eql(StoppedBeating);
+  });
+
+  describe('when toggle is OFF', () => {
+    const featureToggle = { stopBeatingWhenSignalRemainsTheSame: false };
     const heartMonitor = new HeartMonitor(featureToggle);
 
-    it('indicates that heart has stopped beating when signal remains the same', () => {
+    it('indicates that heart is beating when non zero signals remain the same', () => {
       const dataIntervals = [
         { timestamp: '2017-03-16T06:00', signal: 1 },
         { timestamp: '2017-03-16T06:30', signal: 1 },
-        { timestamp: '2017-03-16T07:00', signal: 1 },
-      ];
-
-      expect(heartMonitor.getStatus(dataIntervals)).to.eql(StoppedBeating);
-    });
-
-    it('indicates that heart is beating', () => {
-      const dataIntervals = [
-        { timestamp: '2017-03-16T06:00', signal: 1 },
-        { timestamp: '2017-03-16T06:30', signal: 0 },
         { timestamp: '2017-03-16T07:00', signal: 1 },
       ];
 
